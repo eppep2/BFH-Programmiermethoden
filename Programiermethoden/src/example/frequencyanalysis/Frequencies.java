785f6faa064e38b424f3818ed0c66fb583786392
Samuel Pulfer <samuelpulfer@gmail.com>
2017-11-17 13:39:11 +0100

    Frequencies not yet working

---
+++ Programiermethoden/src/example/frequencyanalysis/Frequencies.java
@@ -0,0 +1,23 @@
+package example.frequencyanalysis;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.xml.soap.Text;
+
+public class Frequencies {
+
+	public static void extract(String text) {
+		Map<String,Integer> result = new TreeMap<String,Integer>();
+		String[] textarray = text.split(" ");
+		for (String word:textarray) {
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
